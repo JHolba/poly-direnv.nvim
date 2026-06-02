43d55ae3efd5ac7b2cfae1fb7de38dae84a18c2e
Jon Holba <jholba@equinor.com>
2026-06-02 15:24:44 +0200

    feat: add flake checks and devShell for formatting/linting
    checks: stylua, selene, alejandra, statix, deadnix
    devShell: all five tools available via nix develop

--- flake.nix
+++ flake.nix
@@ -18,6 +18,41 @@
       };
     });
 
+    checks = forAllSystems (pkgs: {
+      lua-format = pkgs.runCommand "check-lua-format" {} ''
+        ${pkgs.stylua}/bin/stylua --check ${self}/lua
+        touch $out
+      '';
+      lua-lint = pkgs.runCommand "check-lua-lint" {} ''
+        cd ${self} && ${pkgs.selene}/bin/selene --allow-warnings lua/
+        touch $out
+      '';
+      nix-format = pkgs.runCommand "check-nix-format" {} ''
+        ${pkgs.alejandra}/bin/alejandra --check ${self}
+        touch $out
+      '';
+      nix-lint = pkgs.runCommand "check-nix-lint" {} ''
+        ${pkgs.statix}/bin/statix check ${self}
+        touch $out
+      '';
+      nix-deadcode = pkgs.runCommand "check-nix-deadcode" {} ''
+        ${pkgs.deadnix}/bin/deadnix --fail ${self}
+        touch $out
+      '';
+    });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          stylua
+          selene
+          alejandra
+          statix
+          deadnix
+        ];
+      };
+    });
+
     overlays.default = final: _: {
       vimPlugins =
         (final.vimPlugins or {})
